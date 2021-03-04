eebe617e0e38e20f911412042c0b8f0abd7de1d0
Emmanuel Bosquet <bjokac@gmail.com>
2021-03-04 22:49:00 +0100

    changements mineurs

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ var app = new App('https://localhost:5001/api/Artists');
 var bouton = document.getElementById('bouton_liste');
 bouton.addEventListener('click', async () => app.getArtists());
 
-const envoyerArtiste = document.getElementById('nouvelArtiste');
-nouvelArtiste.addEventListener('submit', async (event) => {
-    app.postArtist(event);
-    console.log('ca marche');
+var nouvelArtiste = document.getElementById('nouvelArtiste');
+nouvelArtiste.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event, 'depuis main.js qui marche');
+    app.postArtist(event.target);
 });

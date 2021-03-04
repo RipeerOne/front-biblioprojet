78bf328bb5c8d95fcadcc5f41ede50d8582deed6
Emmanuel Bosquet <bjokac@gmail.com>
2021-03-04 16:00:22 +0100

    it worsk!

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import App from './App.js';
 
 var app = new App();
 var bouton = document.getElementById('bouton_liste');
-bouton.addEventListener('click', () => app.fetchPokemons());
+bouton.addEventListener('click', async () => app.fetchPokemons());

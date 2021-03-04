c10fec83061003576c1b2efa9ea0f18099c77868
Emmanuel Bosquet <bjokac@gmail.com>
2021-03-04 18:15:38 +0100

    liste avec beau CSS

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
 export default class App {
     constructor(url) {
         this.list = document.getElementById('liste');
-        this.url=url;
+        this.url = url;
     }
 
-    async fetchPokemons() {
+    async fetch(method) {
         this.list.innerText = 'On va les attraper tous';
         try {
-            const response = await fetch(this.url).then((response) =>
-                response.json()
-            );
-            const pokemonList = response.data;
+            const response = await fetch(this.url, {
+                // method: method,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                },
+            }).then((response) => response.json());
+            
 
             // crée une chaîne de caractères vide
             var html = '';
 
+            console.log(response);
             // pour chaque élément de la liste (d'index i),
             // ajouter au html <p>Nom du pokémon</p>
-            for (var i = 0; i < pokemonList.length; i++) {
-                html = html + `<p>${pokemonList[i].name} </p>`;
+            for (var i = 0; i < response.length; i++) {
+                console.log(response[i].civilName)
+                html = html + `<li>${response[i].civilName} </li>`;
             }
 
             // écrire le html sur l'élément du DOM qui s'appelle "liste"
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import App from './App.js';
 
-var app = new App("https://api.pokemontcg.io/v2/cards");
+var app = new App("https://localhost:5001/api/Artists");
 var bouton = document.getElementById('bouton_liste');
-bouton.addEventListener('click', async () => app.fetchPokemons());
+bouton.addEventListener('click', async () => app.fetch());

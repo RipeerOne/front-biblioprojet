dc775095f0dbfe25124741c466a122ca5c495f73
Emmanuel Bosquet <bjokac@gmail.com>
2021-03-04 12:39:25 +0100

    la requete fonctionne

--- fetch_api.js
+++ fetch_api.js
@@ -1,17 +1,40 @@
-var liste = document.getElementById("liste")
+var liste = document.getElementById('liste');
 
-// async function afficherListe()
-// {
-    // etape 1 : faire une requête à l'API, attribuer le résultat à la var pokelist
+function creer_liste_en_html(pokemonList) {
+    // crée une chaîne de caractères vide
+    var html = '';
 
-    fetch("https://api.pokemontcg.io/v2/cards")
-    .then(function(response){
-        console.log(response.body)
-    })
-    // etape 2 : lire la liste des pokemon 
-    
-    //
-// }
+    // pour chaque élément de la liste (d'index i),
+    // ajouter au html <p>Nom du pokémon</p>
+    for (var i = 0; i < pokemonList.length; i++) {
+        html = html + `<p>${pokemonList[i].name}</p>`;
+    }
 
-// var boutonElt = document.getElementById("bouton_liste");
-// boutonElt.addEventListener("click", await afficherListe());
+    // écrire le html sur l'élément du DOM qui s'appelle "liste"
+    document.getElementById('liste').innerHTML = html;
+}
+
+// définir l'url de l'API
+var requestURL = 'https://api.pokemontcg.io/v2/cards';
+
+// créer une requête et l'envoyer
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+// récupérer le json de la requête
+request.onload = function () {
+    // on sait déjà que l'API renvoie du json de la forme
+    // {
+    //     data:
+    //         [
+    //             ici la liste
+    //         ]
+    // }
+    // donc on écrit .data
+    var pokemonList = request.response.data;
+
+    // appeler la fonction qui affiche la liste en html
+    creer_liste_en_html(pokemonList);
+};
